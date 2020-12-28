8ef819eb00ecf7685601d97ed8641a6202d14b70
Suhaila <suben@microsoft.com>
2020-12-27 17:15:33 -0800

    exercise passes all tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(yodel) {
 
+    let generator = [];
+
+    for (let i = 1; i <= yodel.length ; i++) {
+        generator.push(yodel[yodel.length - i])
+    }
+
+    return generator.join('');
+    
 }
 
 module.exports = reverseString
