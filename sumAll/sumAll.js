85bd918c40861cc25db1f134fdbdd817a3a800f9
Suhaila <suben@microsoft.com>
2020-12-28 10:29:27 -0800

    solved the puzzle

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    console.log(typeof(a));
+    if (typeof a !== 'number') {
+        return 'ERROR'
+    } else if (typeof b !== 'number') {
+        return 'ERROR'
+    } else if (a < 0 || b < 0) {
+        return 'ERROR'
+    } else {
+        let first = a > b ? b : a;
+        let last = a > b ? a : b;
+        let output = 0;
+        for (let i = first; i <= last ; i++) {
+            output += i
+        }
+        return output
+    }
 }
 
 module.exports = sumAll
