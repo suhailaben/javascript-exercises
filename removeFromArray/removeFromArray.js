c957966d08028922e32165ed7b1a7586580161ce
Suhaila <suben@microsoft.com>
2020-12-27 21:18:00 -0800

    solved the puzzle

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
 const removeFromArray = function() {
+    let output = [];
+    for (let i = 0; i < arguments[0].length ; i++) {
+        output.push(arguments[0][i])
+        for (let x = 1; x < arguments.length; x++) {
+            if (typeof(arguments[x]) === Number || String ) {
+                if (arguments[0][i] === arguments[x]) {
+                    output.pop();
+                    break
+                }
+            }  else {
 
-}
+                for (let y = 0; y < arguments[x].length; y++) {
+                    if (arguments[0][i] === arguments[x][y]) {
+                        output.pop();
+                        break
+                    } else continue
+                }
+            }
+        }
+    } 
+    return output
+ }
 
 module.exports = removeFromArray
