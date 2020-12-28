39f673de3f5a3cd02eb9f202e68f3858e45c619d
Suhaila <suben@microsoft.com>
2020-12-28 14:54:30 -0800

    solved the puzzle

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 !== 0) {
+            return true;
+        } else {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
 }
 
 module.exports = leapYears
