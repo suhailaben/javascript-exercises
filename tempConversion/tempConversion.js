11315fd5ab95f8c6911b816ad75eeb4d72cdfde4
Suhaila <suben@microsoft.com>
2020-12-28 15:14:32 -0800

    solved the puzzle

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fdegrees) {
+    let result = parseFloat(((fdegrees - 32) * 5 / 9).toFixed(1));
+    return result;
 }
 
-const ctof = function() {
-
+const ctof = function(cdegrees) {
+    let result = parseFloat((cdegrees * 9 / 5 + 32).toFixed(1));
+    return result;
 }
 
 module.exports = {
