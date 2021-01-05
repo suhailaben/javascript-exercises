d8542ff3472f52883764f8cc71860f722550dd92
Suhaila <suben@microsoft.com>
2021-01-05 01:21:15 -0800

    well it is done

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,55 @@
-const caesar = function() {
-
+const caesar = function(str, num) {
+    let arr = str.split('');
+    let output = [];
+    if (num > 0) {
+        for (let letter of arr) {
+            let origCode = letter.charCodeAt(0);
+            let newCode = origCode;
+            if ((/[a-z]/).test(letter)) {
+                for (let j = 1; j <= num; j++) {
+                    newCode++;
+                    if (newCode === 123) {
+                        newCode = 97;
+                    }
+                }
+                output.push(String.fromCharCode(newCode));
+            } else if ((/[A-Z]/).test(letter)) {
+                for (let j = 1; j <= num; j++) {
+                    newCode++;
+                    if (newCode === 91) {
+                        newCode = 65;
+                    }
+                }
+                output.push(String.fromCharCode(newCode));
+            } else output.push(letter);
+        }
+        let finalOutput = output.join('');
+        return finalOutput;
+    } else {
+        for (let letter of arr) {
+            let origCode = letter.charCodeAt(0);
+            let newCode = origCode;
+            if ((/[a-z]/).test(letter)) {
+                for (let j = -1; j >= num; j--) {
+                    newCode--;
+                    if (newCode === 96) {
+                        newCode = 122;
+                    }
+                }
+                output.push(String.fromCharCode(newCode));
+            } else if ((/[A-Z]/).test(letter)) {
+                for (let j = -1; j >= num; j--) {
+                    newCode--;
+                    if (newCode === 64) {
+                        newCode = 90;
+                    }
+                }
+                output.push(String.fromCharCode(newCode));
+            } else output.push(letter);
+        }
+        let finalOutput = output.join('');
+        return finalOutput;
+    }
 }
 
 module.exports = caesar
