c65a634cd3a6b65880357af841d4fd76d4a60c17
Suhaila <suben@microsoft.com>
2021-01-04 23:05:10 -0800

    solved the puzzle

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+const palindromes = function(racecar) {
+    let modRacecar = racecar.toLowerCase().replace(/[^A-Za-z]/g, '');
+    let limit = modRacecar.length / 2;
+    let j = modRacecar.length - 1;
+    for (i = 0; i < limit ; i++) {
+        if (modRacecar[i] !== modRacecar[j]) {
+            console.log(modRacecar[i] + modRacecar[j])
+            return false;
+        } 
+        j--
+    } return true;
 }
 
 module.exports = palindromes
