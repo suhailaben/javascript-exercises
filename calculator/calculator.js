eb2e5c93d6dd36857da2a22ee73e9f6bffbc06e7
Suhaila <suben@microsoft.com>
2021-01-04 20:09:42 -0800

    solved the puzzles

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,28 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (arr) {
+	return arr.reduce((a, c) => a + c, 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((a, c) => a * c, 1);
 }
 
-function power() {
-	
+function power(a, b) {
+	return a ** b;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let acc = 1;
+	for (let i = a; i > 0; i--) {
+		acc *= i
+	} return acc;
 }
 
 module.exports = {
