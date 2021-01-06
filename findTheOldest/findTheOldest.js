21b3c36ac5c79121df890b204ba94b3e34b1c823
Suhaila <suben@microsoft.com>
2021-01-06 01:38:10 -0800

    solved it

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-let findTheOldest = function() {
+let findTheOldest = function(list) {
+    let oldest = 0;
+    let oldestName = ''
+    let d = new Date()
+    for (let i = 0; i < list.length; i++) {
+        let death = list[i].yearOfDeath ? list[i].yearOfDeath : d.getFullYear();
+        let age = (death - list[i].yearOfBirth);
+        if (age > oldest) {
+            oldest = age;
+            oldestName = list[i]
+        }
+        }
+        return oldestName;
+    }
 
-}
+
 
 module.exports = findTheOldest
