94217adcbbcaa722eff30880b782ee686228a0b1
Suhaila <suben@microsoft.com>
2021-01-05 19:41:46 -0800

    solved the puzzle

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 1) {
+        return 'OOPS'
+    }
+    let array = [];
+    for (i = 1; i <= number ; i++) {
+        if (i === 1) {
+            array = [1]
+            continue;
+        } else if (i === 2) {
+            array = [1, 1];
+            continue;
+        } else {
+            array[array.length] = (array[array.length - 1] + array[array.length - 2])
+        }
+    }
+    return array[array.length - 1]
 }
 
 module.exports = fibonacci
